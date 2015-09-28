8c2c0058fc0cd3919b0d47867834161502d7d520
bmad4ever <bru_15scp@hotmail.com>
2015-09-29 00:16:48 +0100

    removed stuff to be moved to utilities folder

--- lab1/debug.h
+++
@@ -1,8 +0,0 @@
-// section: should be a definition created by the programmer that must be equal to zero to avoid running the debug code.
-
-#define debug_section(section,code) {\
-if (section != 0)\
-{\
-code\
-}\
-}
